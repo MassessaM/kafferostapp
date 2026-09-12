e68e5f3e119c076bfa72bb11f0691a6e315d0be2
MassessaM <magnus@fallsten.se>
2026-09-12 21:09:45 +0200

    Fix syntax errors in service worker event listeners

--- sw.js
+++ sw.js
@@ -13,13 +13,12 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
   );
-
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys(keys =>
+    caches.keys().then(keys =>
       Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
@@ -27,7 +26,6 @@ self.addEventListener('activate', event => {
       )
     )
   );
-
   self.clients.claim();
 });
 
