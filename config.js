465e72ed4d708e666478273b8b8bdf8f53f503ef
MassessaM <magnus@fallsten.se>
2026-09-12 21:05:10 +0200

    Add MASSE_CONFIG for Google API integration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.MASSE_CONFIG = {
+  googleClientId: '1096267525392-nenun262a8235a7firneacfpak8arr2j.apps.googleusercontent.com',
+  appsScriptDeploymentId: 'AKfycbyVJ-phfEYA5Xf6Npq2NAwzh00fv5tTdNNq8nhZ_fQ14RxQoCcboK0DK3zdIMXu_c-E',
+  scopes: [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/spreadsheets'
+  ].join(' ')
+};
